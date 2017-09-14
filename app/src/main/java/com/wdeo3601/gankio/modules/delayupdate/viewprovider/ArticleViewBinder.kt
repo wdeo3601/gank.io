b28597672b1971a9c8b0f1b6ded7e6d8ee5b08d7
YLMF-1704062119\Administrator <wendong@xmeise.com>
2017-09-14 15:51:09 +0800

    feat：文章中返回图片的，显示图片

--- app/src/main/java/com/wdeo3601/gankio/modules/delayupdate/viewprovider/ArticleViewBinder.kt
+++ app/src/main/java/com/wdeo3601/gankio/modules/delayupdate/viewprovider/ArticleViewBinder.kt
@@ -1,15 +1,18 @@
 package com.wdeo3601.gankio.modules.delayupdate.viewprovider
 
 import android.annotation.SuppressLint
+import android.opengl.Visibility
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.wdeo3601.gankio.R
 import com.wdeo3601.gankio.model.ContentModel
+import com.wdeo3601.gankio.utils.ImageLoader
 import me.drakeet.multitype.ItemViewBinder
 
 /**
@@ -30,6 +33,13 @@ class ArticleViewBinder : ItemViewBinder<ContentModel, ArticleViewBinder.ViewHol
         var publishedAt = item.publishedAt
         publishedAt = publishedAt!!.substring(0, publishedAt.lastIndexOf("T"))
         holder.textviewPublishTime.text = "推送日期：" + publishedAt
+
+        if (item.images != null) {
+            ImageLoader.instance.loadImageUrlCenter(holder.itemView.context, item.images!![0], holder.imageviewArticleImg,null)
+            holder.imageviewArticleImg.visibility = View.VISIBLE
+        } else {
+            holder.imageviewArticleImg.visibility = View.GONE
+        }
         holder.content.text = item.desc
         holder.textviewCreateTime.text = item.createdAt
     }
@@ -37,6 +47,7 @@ class ArticleViewBinder : ItemViewBinder<ContentModel, ArticleViewBinder.ViewHol
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var textviewWho: TextView = itemView.findViewById(R.id.textview_who)
         var textviewPublishTime: TextView = itemView.findViewById(R.id.textview_publish_time)
+        var imageviewArticleImg: ImageView = itemView.findViewById(R.id.imageview_article_img)
         var content: TextView = itemView.findViewById(R.id.textview_desc)
         var textviewCreateTime: TextView = itemView.findViewById(R.id.textview_create_time)
     }
